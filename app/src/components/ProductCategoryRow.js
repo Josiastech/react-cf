316ac958cee24d5c359881dde0f6ea930bb054ca
Josias Chocooj <jchocooj@katch.com>
2016-05-20 11:29:43 -0600

    Initial commit

---
+++ app/src/components/ProductCategoryRow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class ProductCategoryRow extends React.component {
+	constructor(){
+		super();
+	}
+
+	render(){
+		return(
+			<h4>ProductRow</h4>
+		)
+	}
+}
---
+++ app/src/components/ProductTable.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ProductCategoryRow from './ProductCategoryRow'
+import ProductRow from './ProductRow'
+
+export default class ProductTable extends React.Component {
+	constructor(){
+		super();
+	}
+
+	render(){
+		return (
+			<div>
+				<ProductCategoryRow/>
+				<ProductRow/>
+				<ProductRow/>
+				<ProductRow/>
+				<ProductCategoryRow/>
+				<ProductRow/>
+				<ProductRow/>
+				<ProductRow/>
+			</div>
+		)
+	}
+}
